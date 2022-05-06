b062f299ca803d450879769581034d556db995b7
Henry Miller <mail.henrym+git@gmail.com>
2022-05-05 23:47:13 -0400

    basic data fetch working

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Home from '../Home/Home.tsx';
 import './App.css';
 
 export default function App() {
+  let [testData,setTestData] = useState([])
+
+  useEffect(()=>{
+    fetch('https://blocks.flashbots.net/v1/blocks')
+   .then(r=>r.json()).then(r=>setTestData(r))
+  },[])
+
   return (
     <React.Fragment>
       <Home />
