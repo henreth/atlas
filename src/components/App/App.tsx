0ceb2ce74935cf9cfc3911b21081884898122c81
Henry Miller <mail.henrym+git@gmail.com>
2022-05-20 00:18:49 -0400

    updated urls

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from 'react';
 import Home from '../Home/Home.tsx';
 import './App.css';
 
+
+let blocksUrl = 'https://blocks.epheph.com/v1/blocks'
+let flashUrl = 'https://blocks.flashbots.net/v1/blocks'
+let relayUrl = 'http://relay.epheph.com/v1/blocks'
 export default function App() {
   let [testData, setTestData] = useState([])
 
   useEffect(() => {
-    axios.get('https://blocks.flashbots.net/v1/blocks')
+    axios.get(flashUrl)
       .then(r => setTestData(r.data.blocks))
   }, [])
 
