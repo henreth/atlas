5c82bb9719cf935af953ce2a729a7a2f2dd345d0
Henry Miller <mail.henrym+git@gmail.com>
2022-06-19 23:48:20 -0400

    table styling

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,34 +1,37 @@
 import React from 'react'
+import './Table.css'
 
 export default function Table({ data }) {
 
     // gas price: bundle?.gas_used / (10 ** 9)
 
-    let dataToDisplay = data.map(block => {
+    let dataToDisplay = data.slice(0, 15).map(block => {
         return (
-            <tr>
+            <tr >
                 <td>{block.block_number}</td>
                 <td>{block.miner_reward}</td>
                 <td>{block.gas_used}</td>
-                <td>{Math.round(block.miner_reward / block.gas_used / (10**9))} gwei</td>
+                <td>{Math.round(block.miner_reward / block.gas_used / (10 ** 9))} gwei</td>
                 <td>{block.transactions.length}</td>
             </tr>
         )
     })
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Block Number</th>
-                    <th>Miner Reward</th>
-                    <th>Gas Used</th>
-                    <th>Gas Price</th>
-                    <th>Bundles</th>
-                </tr>
-            </thead>
-            <tbody>
-                {dataToDisplay}
-            </tbody>
-        </table>
+        <div className='table-wrapper'>
+            <table className='fl-table'>
+                <thead className='table-header'>
+                    <tr>
+                        <th>Block Number</th>
+                        <th>Miner Reward</th>
+                        <th>Gas Used</th>
+                        <th>Gas Price</th>
+                        <th>Bundles</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataToDisplay}
+                </tbody>
+            </table>
+        </div>
     )
 }
