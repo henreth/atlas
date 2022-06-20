551a087168128cb4806459f004d9a04b928691c5
Henry Miller <mail.henrym+git@gmail.com>
2022-06-19 23:21:27 -0400

    build table head

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,19 @@
+export default function Table() {
+    // gas price: bundle?.gas_used / (10 ** 9)
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Block Number</th>
+                    <th>Miner Reward</th>
+                    <th>Gas Used</th>
+                    <th>Gas Price</th>
+                    <th>Bundles</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+            </tbody>
+        </table>
+    )
+}
